5af2ace344e0825ba433a367fa26f5c9f0c09990
Yordan Halachev <yordan.i.halachev@gmail.com>
2020-02-21 09:46:03 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+variable "gcp_credentials" {}
+
+provider "google" {
+  
+  credentials = var.gcp_credentials
+  project     = "sup-eng-eu"
+  region      = "europe-north1"
+  
+}
+
+provider "null" {}
+
+resource "null_resource" "named" {
+  }
+
+resource "google_compute_instance" "metabase" {
+  name         = "random-name-changed"
+  machine_type = "n1-highmem-2"
+  zone         = "europe-north1-a"
+  
+  tags = ["foo", "bar"]
+  
+  metadata = {
+    foo = "bar"
+  }
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+
